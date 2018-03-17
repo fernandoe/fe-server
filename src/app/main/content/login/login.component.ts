35ae8ffcd592d8f159008488bb857ebb3e00066a
Fernando Espíndola <fer.esp@gmail.com>
2018-03-17 01:50:12 -0300

    Making post to the google.

--- src/app/main/content/login/login.component.ts
+++ src/app/main/content/login/login.component.ts
@@ -1,70 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { fuseAnimations } from '@fuse/animations';
+import {FuseConfigService} from '@fuse/services/config.service';
+import {fuseAnimations} from '@fuse/animations';
+import {ContaService} from '../../../services/conta/conta.service';
 
 @Component({
-    selector   : 'fuse-login',
-    templateUrl: './login.component.html',
-    styleUrls  : ['./login.component.scss'],
-    animations : fuseAnimations
+  selector: 'fuse-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  animations: fuseAnimations
 })
-export class FuseLoginComponent implements OnInit
-{
-    loginForm: FormGroup;
-    loginFormErrors: any;
+export class FuseLoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loginFormErrors: any;
 
-    constructor(
-        private fuseConfig: FuseConfigService,
-        private formBuilder: FormBuilder
-    )
-    {
-        this.fuseConfig.setConfig({
-            layout: {
-                navigation: 'none',
-                toolbar   : 'none',
-                footer    : 'none'
-            }
-        });
+  constructor(private fuseConfig: FuseConfigService,
+              private formBuilder: FormBuilder,
+              private contaService: ContaService) {
 
-        this.loginFormErrors = {
-            email   : {},
-            password: {}
-        };
+    this.fuseConfig.setConfig({
+      layout: {
+        navigation: 'none',
+        toolbar: 'none',
+        footer: 'none'
+      }
+    });
+
+    this.loginFormErrors = {
+      email: {},
+      password: {}
+    };
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.loginForm.valueChanges.subscribe(() => {
+      this.onLoginFormValuesChanged();
+    });
+  }
+
+  onLoginFormValuesChanged() {
+    for (const field in this.loginFormErrors) {
+      if (!this.loginFormErrors.hasOwnProperty(field)) {
+        continue;
+      }
+
+      // Clear previous errors
+      this.loginFormErrors[field] = {};
+
+      // Get the control
+      const control = this.loginForm.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        this.loginFormErrors[field] = control.errors;
+      }
     }
+  }
 
-    ngOnInit()
-    {
-        this.loginForm = this.formBuilder.group({
-            email   : ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required]
-        });
-
-        this.loginForm.valueChanges.subscribe(() => {
-            this.onLoginFormValuesChanged();
-        });
-    }
-
-    onLoginFormValuesChanged()
-    {
-        for ( const field in this.loginFormErrors )
-        {
-            if ( !this.loginFormErrors.hasOwnProperty(field) )
-            {
-                continue;
-            }
-
-            // Clear previous errors
-            this.loginFormErrors[field] = {};
-
-            // Get the control
-            const control = this.loginForm.get(field);
-
-            if ( control && control.dirty && !control.valid )
-            {
-                this.loginFormErrors[field] = control.errors;
-            }
-        }
-    }
+  doLogin() {
+    console.log('==> doLogin');
+    this.contaService.login('fer.esp@gmail.cmo', 'password');
+  }
 }
