6be0bdde41fbcdb112d88e9aefd140c33491fba9
Fernando Espíndola <fernando.espindola@hp.com>
2018-03-19 12:58:53 -0300

    Adicionando dados de exemplos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {TranslateModule} from '@ngx-translate/core';
 import 'hammerjs';
 
-import { FuseModule } from '@fuse/fuse.module';
-import { FuseSharedModule } from '@fuse/shared.module';
+import {FuseModule} from '@fuse/fuse.module';
+import {FuseSharedModule} from '@fuse/shared.module';
 
-import { fuseConfig } from './fuse-config';
+import {fuseConfig} from './fuse-config';
 
-import { AppComponent } from './app.component';
-import { FuseMainModule } from './main/main.module';
-import { FuseSampleModule } from './main/content/sample/sample.module';
-import { LoginModule } from './main/content/login/login.module';
+import {AppComponent} from './app.component';
+import {FuseMainModule} from './main/main.module';
+import {FuseSampleModule} from './main/content/sample/sample.module';
+import {LoginModule} from './main/content/login/login.module';
 import {TokenInterceptor} from './services/conta/token.interceptor';
+import {FuseContactsModule} from "./main/content/contacts/contacts.module";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {FuseFakeDbService} from "./fuse-fake-db/fuse-fake-db.service";
+
 
 const appRoutes: Routes = [
     {
-        path      : '**',
+        path: '**',
         redirectTo: 'sample'
     }
 ];
@@ -28,29 +32,35 @@ const appRoutes: Routes = [
     declarations: [
         AppComponent
     ],
-    imports     : [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
         RouterModule.forRoot(appRoutes),
         TranslateModule.forRoot(),
 
+        InMemoryWebApiModule.forRoot(FuseFakeDbService, {
+            delay: 0,
+            passThruUnknownUrl: true
+        }),
+
         // Fuse Main and Shared modules
         FuseModule.forRoot(fuseConfig),
         FuseSharedModule,
         FuseMainModule,
         FuseSampleModule,
+        FuseContactsModule,
+
         LoginModule
     ],
     providers: [{
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
+        provide: HTTP_INTERCEPTORS,
+        useClass: TokenInterceptor,
+        multi: true
     }],
-    bootstrap   : [
+    bootstrap: [
         AppComponent
     ]
 })
-export class AppModule
-{
+export class AppModule {
 }
