d653773c939908a5ec5c8679df5c3f865dd0cc64
Fernando Espíndola <fernando.espindola@hp.com>
2018-03-20 18:51:36 -0300

    WIP

--- src/app/main/content/clientes/cliente-list/cliente-list.component.ts
+++ src/app/main/content/clientes/cliente-list/cliente-list.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnDestroy, OnInit, TemplateRef, ViewChild, ViewEncapsulation} from '@angular/core';
 import {FilesDataSource} from '../../contacts/contact-list/contact-list.component';
 import {fuseAnimations} from '../../../../../@fuse/animations';
+import {ClientesService} from "../../../../services/cliente/clientes.service";
+import {Subscription} from "rxjs/Subscription";
+import {FuseConfirmDialogComponent} from "../../../../../@fuse/components/confirm-dialog/confirm-dialog.component";
+import {MatDialogRef} from "@angular/material";
 
 @Component({
     selector: 'fe-clientes-list',
@@ -9,15 +13,31 @@ import {fuseAnimations} from '../../../../../@fuse/animations';
     encapsulation: ViewEncapsulation.None,
     animations: fuseAnimations
 })
-export class ClienteListComponent implements OnInit {
+export class ClienteListComponent implements OnInit, OnDestroy {
 
+    @ViewChild('dialogContent') dialogContent: TemplateRef<any>;
+
+    contacts: any;
+    user: any;
     dataSource: FilesDataSource | null;
+    displayedColumns = ['checkbox', 'avatar', 'name', 'email', 'phone', 'jobTitle', 'buttons'];
+    selectedContacts: any[];
     checkboxes: {};
 
-    constructor() {
+    onContactsChangedSubscription: Subscription;
+    onSelectedContactsChangedSubscription: Subscription;
+    onUserDataChangedSubscription: Subscription;
+
+    dialogRef: any;
+
+    confirmDialogRef: MatDialogRef<FuseConfirmDialogComponent>;
+
+    constructor(private clientesService: ClientesService,) {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
     }
 
+    ngOnDestroy(): void {
+    }
 }
