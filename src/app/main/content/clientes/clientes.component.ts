8a1181417ebc85ee45a7360cc81ae624e9e4c80b
Fernando Espíndola <fernando.espindola@hp.com>
2018-03-20 12:55:13 -0300

    Removendo a barra lateral.

--- src/app/main/content/clientes/clientes.component.ts
+++ src/app/main/content/clientes/clientes.component.ts
@@ -1,12 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Subscription} from "rxjs/Subscription";
 import {ContactsService} from "../contacts/contacts.service";
+import {fuseAnimations} from "../../../../@fuse/animations";
 
 @Component({
     selector: 'app-clientes',
     templateUrl: './clientes.component.html',
-    styleUrls: ['./clientes.component.scss']
+    styleUrls: ['./clientes.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations
 })
 export class ClientesComponent implements OnInit {
 
