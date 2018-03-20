aa7e0dbf324f7d7c0e4e7aeadb119db09df190ae
Fernando Espíndola <fernando.espindola@hp.com>
2018-03-20 12:31:27 -0300

    Lista o exemplo de contatos.

--- src/app/main/content/clientes/clientes.component.ts
+++ src/app/main/content/clientes/clientes.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Subscription} from "rxjs/Subscription";
+import {ContactsService} from "../contacts/contacts.service";
 
 @Component({
-  selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.scss']
+    selector: 'app-clientes',
+    templateUrl: './clientes.component.html',
+    styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+    hasSelectedContacts: boolean;
+    searchInput: FormControl;
+    dialogRef: any;
+    onSelectedContactsChangedSubscription: Subscription;
 
-  ngOnInit() {
-  }
+    constructor(private contactsService: ContactsService) {
+        this.searchInput = new FormControl('');
+    }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/main/content/clientes/clientes.module.ts
+++ src/app/main/content/clientes/clientes.module.ts
@@ -2,30 +2,77 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ClienteListComponent} from './cliente-list/cliente-list.component';
 import {RouterModule} from '@angular/router';
-import {MatCheckboxModule, MatIconModule, MatMenuModule, MatTableModule} from '@angular/material';
-import {FormsModule} from '@angular/forms';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatToolbarModule
+} from '@angular/material';
 import {CdkTableModule} from '@angular/cdk/table';
+import {ClientesComponent} from "./clientes.component";
+import {FuseContactsSelectedBarComponent} from "../contacts/selected-bar/selected-bar.component";
+import {FuseContactsMainSidenavComponent} from "../contacts/sidenavs/main/main.component";
+import {FuseContactsComponent} from "../contacts/contacts.component";
+import {FuseContactsContactFormDialogComponent} from "../contacts/contact-form/contact-form.component";
+import {FuseContactsContactListComponent} from "../contacts/contact-list/contact-list.component";
+import {FuseConfirmDialogModule} from "../../../../@fuse/components";
+import {FuseSharedModule} from "../../../../@fuse/shared.module";
+import {ContactsService} from "../contacts/contacts.service";
 
 const routes = [{
     path: 'clientes',
-    component: ClienteListComponent
+    component: ClientesComponent,
+    resolve: {
+        contacts: ContactsService
+    }
 }];
 
 @NgModule({
     declarations: [
-        ClienteListComponent
+        FuseContactsComponent,
+        FuseContactsContactListComponent,
+        FuseContactsSelectedBarComponent,
+        FuseContactsMainSidenavComponent,
+        FuseContactsContactFormDialogComponent,
+
+        ClienteListComponent,
+        ClientesComponent
     ],
     imports: [
         RouterModule.forChild(routes),
         CdkTableModule,
-        FormsModule,
 
+        MatButtonModule,
         MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
         MatIconModule,
+        MatInputModule,
         MatMenuModule,
+        MatRippleModule,
+        MatSidenavModule,
         MatTableModule,
+        MatToolbarModule,
+
+        FuseSharedModule,
+        FuseConfirmDialogModule,
+
         CommonModule
     ],
+    providers: [
+        ContactsService
+    ]
 })
 export class ClientesModule {
+
+    newContact() {
+    }
+
 }
