6aa10ea5fb88c4c1070dc5d699285e84b891d57f
Fernando Espíndola <fer.esp@gmail.com>
2018-03-29 14:21:49 -0300

    Usando o arquivo do environment pra armazenar o endereço dos serviços. (#9)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr: false,
+    contaServerUrl: 'http://52.234.130.35',
+    pessoaServerUrl: 'http://52.234.150.205',
 };
